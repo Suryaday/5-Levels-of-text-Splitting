6396544ffada0176b5761df310dda3837666fe2d
Suryaday <suryadayn@gmail.com>
2026-05-06 12:34:19 +0530

    Added langchain

--- ts.py
+++ ts.py
@@ -1,9 +1,21 @@
+from langchain_text_splitters import CharacterTextSplitter
+
 text = "A Quick brown fox jumps over the lazy dog. This sentence contains all the letters of the alphabet"
 chunks = []
-chunk_size = 25
+chunk_size = 30
 
 for i in range(4, len(text), chunk_size):
     chunk = text[i-4: i+chunk_size]
     chunks.append(chunk)
 
 print(chunks)
+
+#######Langchain############
+
+text_splitter = CharacterTextSplitter(chunk_size=35, chunk_overlap=4, separator="")
+split = text_splitter.create_documents([text])
+
+for i in range(len(split)):
+    print(split[i].page_content)
+
+print(split)
